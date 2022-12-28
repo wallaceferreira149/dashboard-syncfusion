c6bf3c6f8d66bd52edc686250ccfd56c50bcebc9
wallaceferreira149 <wallaceferreira149@gmail.com>
2022-12-28 16:55:20 -0400

    style: :art: add google font and global css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import './globals.css';
 
+import { Open_Sans } from '@next/font/google';
+
+const openSans = Open_Sans({ subsets: ['latin'] });
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br" className={openSans.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
