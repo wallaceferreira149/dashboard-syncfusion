17409f1084bf35624f7a5fd20b89782076650e4e
wallaceferreira149 <wallaceferreira149@gmail.com>
2022-12-28 16:32:42 -0400

    build: :building_construction: add and config storybook with nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-
 export default function Home() {
   return (
     <main className="">
-      <div className="">
-        <p>
-          Next13
-        </p>
-        
+      <div className="bg-green-500">
+        <p>Next13</p>
       </div>
     </main>
-  )
+  );
 }
