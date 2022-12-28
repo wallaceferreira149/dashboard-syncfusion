b68317a4f79e637f5f02e78ea4b0f44eb6bf8dff
wallaceferreira149 <wallaceferreira149@gmail.com>
2022-12-28 16:06:26 -0400

    build: :tada: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className="">
+        <p>
+          Next13
+        </p>
+        
+      </div>
+    </main>
+  )
+}
