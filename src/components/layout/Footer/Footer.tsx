33994402ab843f2b15e2b6638734fd9ad455fe89
wallaceferreira149 <wallaceferreira149@gmail.com>
2022-12-29 18:42:48 -0400

    feat: :sparkles: create footer component

---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+export interface IFooter {
+  sampleProps: string;
+}
+
+export const Footer: React.FC<IFooter> = ({ sampleProps }) => {
+  return (
+    <div className="bg-gray-400 font-bold">
+      <h1 className="text-2xl text-blue-500">Tailwind</h1>
+      {sampleProps}
+    </div>
+  );
+};
