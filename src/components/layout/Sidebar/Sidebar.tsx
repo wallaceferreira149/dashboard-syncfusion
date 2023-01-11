093008e8ac9903c2a055cc67a75427a3bacb758f
wallaceferreira149 <wallaceferreira149@gmail.com>
2023-01-11 17:28:24 -0400

    feat: :construction: function to handle with sidebar auto close
    function handleClose verify  if we have small screen and automatic close sidebar when perform a navigation

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -5,13 +5,20 @@ import { SiShopware } from 'react-icons/si';
 import { useStateContext } from '../../../context/GlobalContext';
 
 import { links } from '../../../data/dummy';
-import { Notification } from '../../common/Notification/Notification';
+import { ToolTip } from '../../common/ToolTip/ToolTip';
 
 export interface ISidebar {}
 
 export const Sidebar: React.FC<ISidebar> = ({}) => {
-  const { setActiveMenu, activeMenu } = useStateContext();
+  const { setActiveMenu, activeMenu, screenSize } = useStateContext();
   const isLinkActive = false;
+
+  const handleCloseSidebar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
+
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
@@ -19,13 +26,13 @@ export const Sidebar: React.FC<ISidebar> = ({}) => {
           <header className="flex justify-between items-center">
             <Link
               href="/"
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSidebar}
               className="flex items-center gap-3 ml-3 mt-4 text-xl font-bold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware />
               <span>Shoppy</span>
             </Link>
-            <Notification content="close">
+            <ToolTip content="close">
               <button
                 type="button"
                 onClick={() =>
@@ -35,7 +42,7 @@ export const Sidebar: React.FC<ISidebar> = ({}) => {
               >
                 <MdOutlineCancel />
               </button>
-            </Notification>
+            </ToolTip>
           </header>
           <menu className="mt-10">
             {links?.map((item) => (
@@ -45,6 +52,8 @@ export const Sidebar: React.FC<ISidebar> = ({}) => {
                   <Link
                     href={link.name}
                     key={link.name}
+                    onClick={handleCloseSidebar}
+                    // prefetch={false}
                     className={
                       isLinkActive
                         ? 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
